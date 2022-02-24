727e9155810a1a4e759de9de0dfd1f014afd5ed4
student-hash <98327528+student-hash@users.noreply.github.com>
2022-02-24 10:27:18 +0300

    Add files via upload

---
+++ kelvin simple.c
@@ -0,0 +1,35 @@
+
+ simple calculator by
+ :kelvin wekesa
+ on feb,2022
+ c99 compiler
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ // variable declaration
+    char name[100];
+    int n1,n2,sum,diff,product;
+    float quot;
+    printf("simple calculator!\n");
+    printf("enter your name :"),
+    gets(name);
+    printf("enter two integers");
+    scanf("%%d",&n1,&n2),
+  //computation
+
+    add= -2;
+    mult= -11;
+    sub= 1;
+    dev=3;
+
+    //outputs
+    printf("-2"),
+    printf("22);
+    printf("21);
+    printf(7);
+
+
+    return 0;
